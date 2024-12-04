49c2f3db2921f03ad161414a9359583faf9a5d68
Alheta Wahyu M. <alheta.matalarens@gmail.com>
2024-12-04 17:11:03 +0700

    init commit

---
+++ src/server.js
@@ -0,0 +1,87 @@
+const express = require("express");
+const app = express();
+const puppeteer = require("puppeteer");
+
+const PORT = 8000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/ssgoogle", async (req, res) => {
+  try {
+    const browser = await puppeteer.launch({
+      args: ["--no-sandbox"],
+    });
+
+    const url = "http://digidb.io/digimon-list/";
+
+    const page = await browser.newPage();
+    await page.goto(url);
+
+    // Targeting the DOM Nodes that contain the Digimon names
+    const digimonNames = await page.$$eval(
+      "#digiList tbody tr td:nth-child(2) a",
+      function (digimons) {
+        // Mapping each Digimon name to an array
+        return digimons.map(function (digimon) {
+          return digimon.innerText;
+        });
+      }
+    );
+
+    console.log(digimonNames);
+
+    const image = await page.screenshot({
+      fullPage: true,
+      path: "screenshot.png",
+    });
+
+    await browser.close();
+    res.set("Content-Type", "image/png");
+    res.send(image);
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+app.get("/otakudesu", async (req, res) => {
+  try {
+    const browser = await puppeteer.launch({
+      args: ["--no-sandbox"],
+    });
+
+    const url = "https://otakudesu.cloud/ongoing-anime/";
+
+    const page = await browser.newPage();
+    await page.goto(url);
+
+    // Targeting the DOM Nodes that contain the Digimon names
+    const selector =
+      "#venkonten .vezone .venser .venutama .rseries .rapi .venz ul li";
+    const digimonNames = await page.$$eval(selector, (results) =>
+      results.map((result) => ({
+        title: result.querySelector(".jdlflm").innerText,
+        episode: result.querySelector(".epz").innerText.trim(),
+        image: result.querySelector(".thumbz img").getAttribute("src"),
+      }))
+    );
+
+    console.log(digimonNames);
+
+    const image = await page.screenshot({
+      fullPage: true,
+      path: "screenshot2.png",
+    });
+
+    await browser.close();
+    // res.set("Content-Type", "image/png");
+    res.send(digimonNames);
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Running on port: ${PORT}`);
+});
